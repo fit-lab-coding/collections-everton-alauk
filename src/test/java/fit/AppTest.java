4af94f3c7caf8b171d06e9b5265a1ecb50d3799d
synthdecor <everton.freire@aluno.faculdadeimpacta.com.br>
2021-02-03 22:51:50 -0300

    ok

--- src/test/java/fit/AppTest.java
+++ src/test/java/fit/AppTest.java
@@ -13,47 +13,8 @@ import java.util.Map;
 import org.junit.Test;
 
 import fit.core.AttendanceHelper;
-import fit.core.ListUtils;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * validate if class can sort an regular array
-     */
-    @Test
-    public void shouldSortList() {
-        // arrange
-        List<String> names = new ArrayList<>() {
-            {
-                add("Emilio");
-                add("Maria");
-                add("José");
-            }
-        };
-
-        List<String> expectedResult = new ArrayList<>() {
-            {
-                add("Emilio");
-                add("José");
-                add("Maria");
-            }
-        };
-
-        // comparator
-        Comparator<String> comp = (String one, String another) -> {
-            if (one == null)
-                return -1;
-            return (one.compareTo(another));
-        };
-
-        // act
-        List<String> actualResult = ListUtils.<String>sort(names, comp);
-
-        // assert
-        assertArrayEquals(expectedResult.toArray(), actualResult.toArray());
-    }
 
     @Test
     public void shouldGenerateRandomAttendanceList() {
@@ -70,7 +31,7 @@ public class AppTest {
         //assert
 
         //same size (due to parameters sent to random method)
-        assertEquals(attendanceListResult.size(), attendanceListResult.size());
+        assertEquals(attendanceListResult.size(), attendanceListExpected.size());
 
         //same percentile of 'true' values due to parameters sent to random method
         assertEquals(
